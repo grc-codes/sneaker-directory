1e0477de2919ecd18a258e8a32c11c945235cb59
Andrew Garcia <grc.codes@gmail.com>
2020-05-28 13:13:19 -0400

    created hero section and product preview section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,393 @@
     <section id="landing">
         <div class="hero">
             <div class="hero-img">
-                A
+                <img src="/img/nike-sb-brazil-mobile.jpg" />
             </div>
-
             <div class="hero-text">
-                B 
+                <div class="box">
+                    <h4 class="box-header">NIKE</h4>
+                    <h5 class="box-title">Nike Dunk Low "Brazil"</h5>
+                    <p class="box-text">The classic Dunk Low colorway that originally released in 2001 is back.</p>
+                    <a class="btn" href="#">
+                        SHOP NOW
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="collection-preview">
+            <div class="collection-preview-header">
+                <div class="collection-name">
+                    New Arrivals
+                </div>
+                <a class="collection-link" href="#">See All<i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="collection-preview-grid">
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="collection-preview">
+            <div class="collection-preview-header">
+                <div class="collection-name">
+                    New Arrivals
+                </div>
+                <a class="collection-link" href="#">See All<i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="collection-preview-grid">
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="collection-preview">
+            <div class="collection-preview-header">
+                <div class="collection-name">
+                    New Arrivals
+                </div>
+                <a class="collection-link" href="#">See All<i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="collection-preview-grid">
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="collection-preview">
+            <div class="collection-preview-header">
+                <div class="collection-name">
+                    New Arrivals
+                </div>
+                <a class="collection-link" href="#">See All<i class="fa fa-angle-right"></i></a>
+            </div>
+            <div class="collection-preview-grid">
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
+                <a href="#" class="sneaker-preview">
+                    <div class="image-container">
+                        <img src="/img/product-preview.png" />
+                    </div>
+                    <div class="sneaker-name">
+                        Yeezy Boost 700 MNVM
+                    </div>
+                    <div class="sneaker-colorway">
+                        "Triple Black"
+                    </div>
+                    <div class="sneaker-price">
+                        $429.00
+                    </div>
+                </a>
             </div>
         </div>
     </section>
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="subscribe">
+        <div class="subscribe-text">
+            Be the first to hear about new releases, special offers and more.
+        </div>
+        <div class="input-container">
+            <div class="input-box">
+                <input
+                    type="email"
+                    autocorrect="off"
+                    spellcheck="false"
+                    name="email"
+                    placeholder="Email Address"
+                >
+            </div>
+            <button type="submit" title="Sign Up" class="sign-up">Sign Up</button>
+        </div>
+    </div>
+</footer>
