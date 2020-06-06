5cf297d13464d6a335722b9bc8cb18d0e3989038
Andrew Garcia <grc.codes@gmail.com>
2020-06-05 21:40:58 -0400

    mobile and tablet responsiveness finished. added menu button functionality

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,69 @@
 <header>
-    <div class="navbar">
+    <div class="navbar menu-open">
         <div class="menu-icon">
-            <i class="fa fa-bars"></i>
+            <i class="fas fa-bars" id="menu-toggle"></i>
         </div>
         <img src="/img/stockx_homepage_logo_dark.svg" class="logo">
         <div class="search-icon">
-            <i class="fa fa-search"></i>
+            <i class="fas fa-search"></i>
         </div>
     </div>
 </header>
+<div id="menu-container" style="display: none">
+    <ul>
+        <li>
+            <a href="#">
+                <div class="text">Nike</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Jordan</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Adidas</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Yeezy</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Shoes</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Women</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Streetwear</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Trophy Case</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <div class="text">Stadium Goods</div>
+                <i class="fas fa-chevron-right"></i>
+            </a>
+        </li>
+    </ul>
+</div>
